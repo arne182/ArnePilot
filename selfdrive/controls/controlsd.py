32c5bd4ced52dee0eebb5480f6c8737ab7912b57
Shane Smiskol <shane@smiskol.com>
2019-03-31 23:51:26 -0500

    Revert profile for non-pedal

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -262,7 +262,7 @@ def state_control(plan, path_plan, CS, CP, state, events, v_cruise_kph, v_cruise
     elif CS.gasbuttonstatus == 1:
       CP.gasMaxV = [0.7, 0.9, 0.9]
     elif CS.gasbuttonstatus == 2:
-      CP.gasMaxV = [0.2, 0.3, 0.5]
+      CP.gasMaxV = [0.2, 0.2, 0.2]
 
   cur_time = sec_since_boot()  # TODO: This won't work in replay
   mpc_time = plan.l20MonoTime / 1e9
