57a4bb8d63cb3c3b42293afce630cafa9772f3ca
Shane <shane@smiskol.com>
2019-11-18 15:28:46 -0600

    testing submaster

--- selfdrive/phantom.py
+++ selfdrive/phantom.py
@@ -10,7 +10,8 @@ class Phantom:
   def __init__(self, timeout=1.):
     context = zmq.Context()
     self.op_params = opParams()
-    self.phantom_Data_sock = messaging_arne.sub_sock('phantomData', conflate=True)
+    self.sm = messaging_arne.SubMaster('phantomData')
+    # self.phantom_Data_sock = messaging_arne.sub_sock('phantomData', conflate=True)
     self.data = {"status": False, "speed": 0.0}
     self.last_receive_time = time.time()
     self.last_phantom_data = {"status": False, "speed": 0.0}
@@ -20,20 +21,25 @@ class Phantom:
       self.mod_sshd_config()
 
   def update(self):
-    phantom_data = messaging_arne.recv_one_or_none(self.phantom_Data_sock)
-    if phantom_data is not None:
-      self.data = {"status": phantom_data.phantomData.status, "speed": phantom_data.phantomData.speed,
-                   "angle": phantom_data.phantomData.angle, "time": phantom_data.phantomData.time}
-      self.last_phantom_data = dict(self.data)
-      self.last_receive_time = time.time()
-      self.to_disable = not self.data["status"]
-    else:
-      if self.to_disable:  # if last message is status: False, disable phantom mode, also disable by default
-        self.data = {"status": False, "speed": 0.0}
-      elif time.time() - self.last_receive_time >= self.timeout:  # lost connection, don't disable. keep phantom on but set speed to 0
-        self.data = {"status": True, "speed": 0.0, "angle": 0.0, "time": 0.0, "lost_connection": True}
-      else:  # if waiting between messages from app, message becomes none, this uses the data from last message
-        self.data = dict(self.last_phantom_data)
+    self.sm.update(0)
+    phantom_data = self.sm['phantomData']
+    updated = self.sm.updated['phantomData']
+    print('Updated: {}'.format(updated))
+    print([phantom_data.status, phantom_data.speed, phantom_data.angle, phantom_data.time])
+    # phantom_data = messaging_arne.recv_one_or_none(self.phantom_Data_sock)
+    # if phantom_data is not None:
+    #   self.data = {"status": phantom_data.phantomData.status, "speed": phantom_data.phantomData.speed,
+    #                "angle": phantom_data.phantomData.angle, "time": phantom_data.phantomData.time}
+    #   self.last_phantom_data = dict(self.data)
+    #   self.last_receive_time = time.time()
+    #   self.to_disable = not self.data["status"]
+    # else:
+    #   if self.to_disable:  # if last message is status: False, disable phantom mode, also disable by default
+    #     self.data = {"status": False, "speed": 0.0}
+    #   elif time.time() - self.last_receive_time >= self.timeout:  # lost connection, don't disable. keep phantom on but set speed to 0
+    #     self.data = {"status": True, "speed": 0.0, "angle": 0.0, "time": 0.0, "lost_connection": True}
+    #   else:  # if waiting between messages from app, message becomes none, this uses the data from last message
+    #     self.data = dict(self.last_phantom_data)
 
   def mod_sshd_config(self):
     # this disables dns lookup when connecting to EON to speed up commands from phantom app, reboot required
