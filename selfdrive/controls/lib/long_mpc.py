15dbf509d6ee4cdbdf15f5999c1759bbc129e287
Shane <shane@smiskol.com>
2019-11-18 13:57:45 -0600

    make values negative

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -120,7 +120,7 @@ class LongitudinalMpc():
       TR = interp(self.v_ego, x, y)
 
     if self.v_lead is not None:  # since the new mpc now handles braking nicely, simplify mods
-      x = [2.68, 2.1, 1.26, 0.61, 0, 0.61, 1.26, 2.1, 2.68]  # relative velocity values todo: add back negative values to get farther away from lead when it's slower
+      x = [-2.68, -2.1, -1.26, -0.61, 0, 0.61, 1.26, 2.1, 2.68]  # relative velocity values
       y = [0.272, 0.154, 0.053, 0.017, 0, -0.017, -0.053, -0.154, -0.272]  # modification values
       TR_mod = interp(self.v_lead - self.v_ego, x, y)  # quicker acceleration/don't brake when lead is overtaking
 
