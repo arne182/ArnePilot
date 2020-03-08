78115731730ccf18526d620a45fd11b982ead342
Shane <shane@smiskol.com>
2020-03-07 18:08:31 -0600

    test

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -231,7 +231,7 @@ static void ui_init_vision(UIState *s, const VisionStreamBufs back_bufs,
 }
 
 bool df_button_clicked(int touch_x, int touch_y) {
-  if ((1525 <= touch_x <= 1700) && (touch_y >= 830)) {
+  if ((800 <= touch_x <= 1920) && (touch_y >= 830)) {
     return true;
   }
   return false;
