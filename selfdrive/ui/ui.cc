a2c02fd479f70156bcd2c736c6aa090961f1e0e8
Shane <shane@smiskol.com>
2020-03-07 18:28:13 -0600

    test

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -1043,7 +1043,9 @@ int main(int argc, char* argv[]) {
 
     //dfButton manager  // code below thanks to kumar: https://github.com/arne182/openpilot/commit/71d5aac9f8a3f5942e89634b20cbabf3e19e3e78
     if (s->awake && s->vision_connected && s->active_app == cereal_UiLayoutState_App_home && s->status != STATUS_STOPPED) {
-      std::cout << "x: " << touch_x << " y: " << touch_y << std::endl;
+      if (touched == 1){
+        std::cout << "x: " << touch_x << " y: " << touch_y << std::endl;
+      }
       if (df_button_clicked(touch_x, touch_y)) {
         std::cout << "df button clicked!\n";
         s->scene.dfButtonStatus++;
