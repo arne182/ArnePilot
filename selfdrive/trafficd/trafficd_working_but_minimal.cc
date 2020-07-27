7a87c71f93677127137e02230d9a9bbc0f901ec4
Shane Smiskol <shane@smiskol.com>
2020-07-27 13:28:55 -0500

    try compiling the full trafficd

---
+++ selfdrive/trafficd/trafficd_working_but_minimal.cc
@@ -0,0 +1,41 @@
+#include "traffic.h"
+
+using namespace std;
+
+std::unique_ptr<zdl::SNPE::SNPE> snpe;
+
+zdl::DlSystem::Runtime_t checkRuntime() {
+    static zdl::DlSystem::Version_t Version = zdl::SNPE::SNPEFactory::getLibraryVersion();
+    static zdl::DlSystem::Runtime_t Runtime;
+    std::cout << "SNPE Version: " << Version.asString().c_str() << std::endl; //Print Version number
+    if (zdl::SNPE::SNPEFactory::isRuntimeAvailable(zdl::DlSystem::Runtime_t::GPU)) {
+        Runtime = zdl::DlSystem::Runtime_t::GPU;
+    } else {
+        Runtime = zdl::DlSystem::Runtime_t::CPU;
+    }
+    return Runtime;
+}
+
+void initializeSNPE(zdl::DlSystem::Runtime_t runtime) {
+    std::unique_ptr<zdl::DlContainer::IDlContainer> container;
+    container = zdl::DlContainer::IDlContainer::open("../../models/traffic_model.dlc");
+    zdl::SNPE::SNPEBuilder snpeBuilder(container.get());
+    snpe = snpeBuilder.setOutputLayers({})
+                      .setRuntimeProcessor(runtime)
+                      .setUseUserSuppliedBuffers(false)
+                      .setPerformanceProfile(zdl::DlSystem::PerformanceProfile_t::HIGH_PERFORMANCE)
+                      .setCPUFallbackMode(true)
+                      .build();
+}
+
+void initModel() {
+    zdl::DlSystem::Runtime_t runt=checkRuntime();
+    initializeSNPE(runt);
+}
+
+int main(){
+    //usleep(5000000);
+    //set_realtime_priority(2);
+    initModel(); // init model
+    printf("here!\n");
+}
