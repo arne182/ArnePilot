528dbe0094f49431063dd3aa1171852cb8b72494
Shane Smiskol <shane@smiskol.com>
2020-07-27 13:46:23 -0500

    debug

--- selfdrive/trafficd/trafficd.cc
+++ selfdrive/trafficd/trafficd.cc
@@ -52,8 +52,8 @@ std::unique_ptr<zdl::DlSystem::ITensor> loadInputTensor(std::unique_ptr<zdl::SNP
     const auto &strList_opt = snpe->getInputTensorNames();
 
     if (!strList_opt) throw std::runtime_error("Error obtaining Input tensor names");
-//    const auto &strList = *strList_opt;
-//    assert (strList.size() == 1);
+    const auto &strList = *strList_opt;
+    assert (strList.size() == 1);
 //
 //    const auto &inputDims_opt = snpe->getInputDimensions(strList.at(0));
 //    const auto &inputShape = *inputDims_opt;
