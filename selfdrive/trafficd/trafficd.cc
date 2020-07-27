a3e26e9711a4456d1f4308798e64d862d3554721
Shane Smiskol <shane@smiskol.com>
2020-07-27 13:43:31 -0500

    debug

--- selfdrive/trafficd/trafficd.cc
+++ selfdrive/trafficd/trafficd.cc
@@ -49,19 +49,19 @@ void initializeSNPE(zdl::DlSystem::Runtime_t runtime) {
 
 std::unique_ptr<zdl::DlSystem::ITensor> loadInputTensor(std::unique_ptr<zdl::SNPE::SNPE> &snpe, std::vector<float> inputVec) {
     std::unique_ptr<zdl::DlSystem::ITensor> input;
-    const auto &strList_opt = snpe->getInputTensorNames();
-
-    if (!strList_opt) throw std::runtime_error("Error obtaining Input tensor names");
-    const auto &strList = *strList_opt;
-    assert (strList.size() == 1);
-
-    const auto &inputDims_opt = snpe->getInputDimensions(strList.at(0));
-    const auto &inputShape = *inputDims_opt;
-
-    input = zdl::SNPE::SNPEFactory::getTensorFactory().createTensor(inputShape);
-
-    /* Copy the loaded input file contents into the networks input tensor. SNPE's ITensor supports C++ STL functions like std::copy() */
-    std::copy(inputVec.begin(), inputVec.end(), input->begin());
+//    const auto &strList_opt = snpe->getInputTensorNames();
+//
+//    if (!strList_opt) throw std::runtime_error("Error obtaining Input tensor names");
+//    const auto &strList = *strList_opt;
+//    assert (strList.size() == 1);
+//
+//    const auto &inputDims_opt = snpe->getInputDimensions(strList.at(0));
+//    const auto &inputShape = *inputDims_opt;
+//
+//    input = zdl::SNPE::SNPEFactory::getTensorFactory().createTensor(inputShape);
+//
+//    /* Copy the loaded input file contents into the networks input tensor. SNPE's ITensor supports C++ STL functions like std::copy() */
+//    std::copy(inputVec.begin(), inputVec.end(), input->begin());
     return input;
 }
 
