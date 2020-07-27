2013bd788308fd375009c4c3e68749cd49f4ab52
Shane Smiskol <shane@smiskol.com>
2020-07-27 13:24:32 -0500

    add snpe init code and print version

--- selfdrive/trafficd/trafficd.cc
+++ selfdrive/trafficd/trafficd.cc
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+zdl::DlSystem::Runtime_t checkRuntime() {
+    static zdl::DlSystem::Version_t Version = zdl::SNPE::SNPEFactory::getLibraryVersion();
+    static zdl::DlSystem::Runtime_t Runtime;
+    std::cout << "SNPE Version: " << Version.asString().c_str() << std::endl; //Print Version number
+    if (zdl::SNPE::SNPEFactory::isRuntimeAvailable(zdl::DlSystem::Runtime_t::GPU)) {
+        Runtime = zdl::DlSystem::Runtime_t::GPU;
+    } else {
+        Runtime = zdl::DlSystem::Runtime_t::CPU;
+    }
+    return Runtime;
+}
+
+void initModel() {
+    zdl::DlSystem::Runtime_t runt=checkRuntime();
+    initializeSNPE(runt);
+}
+
 int main(){
+    signal(SIGINT, (sighandler_t)set_do_exit);
+    signal(SIGTERM, (sighandler_t)set_do_exit);
+    int err;
+    //usleep(5000000);
+    //set_realtime_priority(2);
+    initModel(); // init model
     printf("here!\n");
 }
