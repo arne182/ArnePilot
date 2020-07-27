b731671742e9a50bd3293e160d4b5eb769158d84
Shane Smiskol <shane@smiskol.com>
2020-07-27 13:45:07 -0500

    debug

--- selfdrive/trafficd/trafficd.cc
+++ selfdrive/trafficd/trafficd.cc
@@ -49,8 +49,8 @@ void initializeSNPE(zdl::DlSystem::Runtime_t runtime) {
 
 std::unique_ptr<zdl::DlSystem::ITensor> loadInputTensor(std::unique_ptr<zdl::SNPE::SNPE> &snpe, std::vector<float> inputVec) {
     std::unique_ptr<zdl::DlSystem::ITensor> input;
-//    const auto &strList_opt = snpe->getInputTensorNames();
-//
+    const auto &strList_opt = snpe->getInputTensorNames();
+
 //    if (!strList_opt) throw std::runtime_error("Error obtaining Input tensor names");
 //    const auto &strList = *strList_opt;
 //    assert (strList.size() == 1);
