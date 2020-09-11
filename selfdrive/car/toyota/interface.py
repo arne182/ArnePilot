2a6e334a0c4cb4435c4705df208f53dbfe943c0c
Thomas Pichard <pichard.thomas@icloud.com>
2020-09-11 11:09:36 +0200

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -305,11 +305,11 @@ class CarInterface(CarInterfaceBase):
       stop_and_go = True
       ret.safetyParam = 100
       ret.wheelbase = 2.60
-      ret.steerRatio = 13.9
+      ret.steerRatio = 12.5
       tire_stiffness_factor = 0.444
       ret.mass = 3108 * CV.LB_TO_KG + STD_CARGO_KG  # mean between min and max
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.5], [0.1]]
-      ret.lateralTuning.pid.kfV = [0.00007818594]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.06]]
+      ret.lateralTuning.pid.kfV = [0.00006]
 
     elif candidate == CAR.LEXUS_NXH:
       stop_and_go = True
