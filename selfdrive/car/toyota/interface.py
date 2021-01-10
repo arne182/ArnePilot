ca06e7161d583a6d87b5d758d0b1623b296a5911
SCshredder17 <57152954+SCshredder17@users.noreply.github.com>
2021-01-10 14:17:13 -0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -267,7 +267,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 15.33
       tire_stiffness_factor = 0.996  # not optimized yet
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.steerActuatorDelay = 0.4 
+      ret.steerActuatorDelay = 0.5 
       ret.steerLimitTimer = 5.0
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGainBP = [18, 22, 26]
